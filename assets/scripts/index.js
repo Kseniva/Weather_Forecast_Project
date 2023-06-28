45a5e1b85b91d6dc14d926f4f2dd811244deaad9
Mac <mac@MacBook-Air-Mac.local>
2023-06-29 02:56:39 +0900

    weatherapi and search

---
+++ assets/scripts/index.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('.search');
+const input = document.querySelector('.searchTerm');
+const searchBtn = document.querySelector('.searchButton');
+let city;
+
+
+// создаем функцию для получения данных о погоде
+ function getWeather(city) {
+  const weatherApiKey = '45ae069833aa4900af474813232206'; // записываем в переменную ключ Weather API
+  // записываем в переменную ссылку на API
+  const url = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}&days=5&aqi=yes`;
+
+    fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // проверяем доступность необходимых данных в консоли
+      console.log(data.location.name);
+      console.log(data.location.localtime);
+      console.log(data.current.temp_c);
+      console.log(data.current.temp_f);
+      console.log(data.current.humidity);
+      console.log(data.current.pressure_mb); // указано в мБ, необходимо перевести в мм рт.ст.
+      console.log(data.current.uv);
+      console.log(data.current.wind_kph);
+      console.log(data.current.feelslike_c);
+      console.log(data.current.feelslike_f);
+      console.log(data.current.wind_kph);
+      console.log(data.current.vis_km);
+      console.log(data.current.condition);
+  })
+  .catch(error => console.log (error));
+ }
+// создаем обработчик отправки формы
+form.onsubmit = function (evt) {
+    evt.preventDefault();
+    city = input.value.trim();
+    console.log(city);
+    getWeather(city)
+  }
